fd0dcfa28c0a01ff905aef63cbfcc9c1b0d6873a
Ersilda Fraholli <ersildafraholli@fshnstudent.info>
2025-03-17 20:45:35 +0100

    forms and csrf potection

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,27 @@
 
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function(){
     return redirect()->route('tasks.index');
 });
 
-Route::get('tasks/', function () {
+Route::get('/tasks', function () {
     return view('index', [
         'tasks' => \App\Models\Task::latest()->get()
     ]);
 })->name('tasks.index');
 
-Route::get('tasks/{id}', function($id){
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{id}', function($id){
     return view('show', ['task' =>  \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 Route::fallback(function(){
     return "Still got somewhere";
