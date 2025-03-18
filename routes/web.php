47e97d94b2801c75a3facc19f3b1eedcfd30f6cc
Ersilda Fraholli <ersildafraholli@fshnstudent.info>
2025-03-18 10:03:53 +0100

    update task form

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::get('/', function(){
+Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
@@ -17,15 +17,19 @@ Route::get('/tasks', function () {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}', function($id){
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', ['task' => Task::findOrFail($id)]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
-        'title'=>'required|max:255',
-        'description'=>'required',
-        'long_description'=>'required'
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
     ]);
 
     $task = new Task;
@@ -34,9 +38,25 @@ Route::post('/tasks', function (Request $request) {
     $task->long_description = $data['long_description'];
     $task->save();
 
-    return redirect()->route('tasks.show', ['id'=> $task->id])->with('success', 'Task created successfully!');
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::fallback(function(){
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task updated successfully!');
+})->name('tasks.update');
+
+Route::fallback(function () {
     return "Still got somewhere";
 });
